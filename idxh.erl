c0350f406ae07696ec1224c66e7c98c62f4c5685
Jon Meredith <jmeredith@basho.com>
2011-09-01 21:51:41 -0600

    Updated idxh for interface changes - PBC still broken I think.

--- idxh.erl
+++ idxh.erl
@@ -44,4 +44,18 @@ search() ->
 search(B, SI, SK) ->
     {ok,C} = riak:local_client(),
     io:format("Search ~p / ~p = ~p\n ~p\n",
-              [B, SI, SK, C:get_index(B, [{eq, SI, [SK]}])]).
+              [B, SI, SK, C:get_index(B, [{eq, SI, SK}])]).
+
+search(B, SI, SK1, SK2) ->
+    {ok,C} = riak:local_client(),
+    io:format("Search ~p ~p [~p,~p] =\n ~p\n",
+              [B, SI, SK1, SK2, C:get_index(B, [{gte, SI, SK1},{lte, SI, SK2}])]).
+
+searchpbc() ->
+    searchpbc(<<"b">>, <<"idx1_bin">>, <<"x">>).
+
+
+searchpbc(B, SI, SK) ->
+    {ok, Pid} = riakc_pb_socket:start_link("127.0.0.1", 8081),
+    io:format("Search ~p / ~p = ~p\n ~p\n",
+              [B, SI, SK, riakc_pb_socket:get_index(Pid, B, SI, SK)]).
